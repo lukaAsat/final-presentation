50a48130586618ab92a17ab2ccf306314c1509d0
luka <lukaasatiani7@gmail.com>
2023-06-02 20:50:16 +0400

    final project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Final-Project
+Digitaledu final project
---
+++ script.js
@@ -0,0 +1,9 @@
+let mobileMenu = document.querySelector(".ul");
+let burger = document.querySelector(".fa-bars");
+let xmark = document.querySelector(".fa-xmark");
+
+
+
+burger.addEventListener("click", ()=>{
+mobileMenu.classList.add("appear_diss");
+});
